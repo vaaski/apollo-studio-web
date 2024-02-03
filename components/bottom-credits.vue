36a826c42dec8ff22e5e87510f35a588c7a80346
vaaski <admin@vaa.ski>
2024-02-03 20:09:44 +0100

    shorten bottom-credits

--- components/bottom-credits.vue
+++ components/bottom-credits.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="bottom-credits">
     <span>
-      website
-      <a href="https://github.com/vaaski/apollo-studio-web" class="built">built</a> with
-      <a href="https://nuxt.com/" class="nuxt">nuxt</a> by
+      <a href="https://github.com/vaaski/apollo-studio-web" class="built">
+        website built
+      </a>
+      by
       <a href="https://vaaski.dev" class="vaaski">vaaski</a>
     </span>
   </div>
@@ -17,18 +18,8 @@
   opacity: 0.5;
   user-select: none;
 
-  .wrap {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
   .built {
     color: var(--color-text-bright);
-  }
-
-  .nuxt {
-    color: #00d777;
   }
 
   .vaaski {
